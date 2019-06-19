a446f02ef37a76af5074171344c6b416ddb2ae2a
gomesbreno <brenodmelogomes@outlook.com>
2019-06-19 19:27:11 -0300

    Criar um crud da instancia cliente, que atualmente é a penas criado no sistema (perfectiva).

--- controller/ClientController.php
+++ controller/ClientController.php
@@ -1,24 +1,66 @@
 <?php
-
-Use Model\Page;
-use Model\Client;
-use Model\User;
-
-$app->get('/clients', function(){
-
-    $user = new User;
-
-    $user->logout();
-
-    $page = new Page([
-        'title'=>"Clientes",
-        'order'=>'',
-        'menu'=>'',
-        'district'=>'',
-        'client'=>'active',
-        'clientList'=>Client::listAll()
-    ]);
-
-    $page->setTpl('client');
-
-});
+    
+    Use Model\Page;
+    use Model\Client;
+    use Model\User;
+    use Model\District;
+    
+    $app->get('/clients', function(){
+              
+              $user = new User;
+              
+              $user->logout();
+              
+              $page = new Page([
+                               'title'=>"Clientes",
+                               'order'=>'',
+                               'menu'=>'',
+                               'district'=>'',
+                               'client'=>'active',
+                               'clientList'=>Client::listAll()
+                               ]);
+              
+              $page->setTpl('client');
+              
+              });
+    
+    $app->get("/clients/delete/:cd_cliente", function($cd_cliente){
+              
+              $cliente = new Client;
+              
+              $cliente->delete((int)$cd_cliente);
+              
+              header("Location: /clients");
+              exit;
+              
+              });
+    
+    $app->get('/clients/update/:cd_cliente', function ($cd_cliente) {
+              
+              $cliente = new Client;
+              
+              $cliente->get((int)$cd_cliente);
+              
+              $page = new Page([
+                               'title' => 'Alterar dados',
+                               'order' => '',
+                               'menu' => '',
+                               'client'=>'active',
+                               'district'=>''
+                               ]);
+              
+              $page->setTpl('updateAddress2', [
+                            'clientData' => $cliente->getValues(),
+                            'getDistrict'=>District::listAll()
+                            ]);
+              
+              });
+    
+    $app->post('/clients/update/:cd_cliente', function ($cd_cliente) {
+               
+               Client::updateAddress($cd_cliente, $_POST["nm_logradouro"], $_POST["nm_cliente"], $_POST["nr_casa"], $_POST["nm_bloco"], $_POST["cd_bairro"]);
+               
+               header("Location: /clients");
+               exit;
+               
+               });
